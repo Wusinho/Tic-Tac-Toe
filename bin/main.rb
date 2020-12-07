94f8733c37b3c53b77a4ba4d1c2bc48373fdf3d2
Heber <heberlazo@gmail.com>
2020-12-07 14:27:24 -0500

    Add Main file interactions

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,37 @@
 #!/usr/bin/env ruby
-puts 'Hello world!'
+
+class InitGame
+   
+
+    def Dialogo
+        # Input Players
+        puts 'Whats your name?'
+        @Player_1 = gets.chomp
+
+        puts "Hello #{@Player_1} "
+        puts 'Whats the name of your partner?'
+        @Player_2 = gets.chomp
+
+        puts "Hello #{@Player_2} "
+
+        # Grid Size
+        puts 'Pick a grid size equal or greater than 3'
+        @Pick_size = gets.chomp
+
+        # Building grid to play
+        grilla = Grid.new(@Pick_size) 
+        grilla.create_grid
+        grilla.print_table
+
+        # Turns Methods
+
+        turn = Turn.new(@Player_1, @Player_2)
+        turn.call_turn
+        
+        # Condition to win
+        win = Win.new(@Player_1, @Player_2)
+        win.call_winner
+
+
+    end
+end
