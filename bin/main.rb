0875695b6ba0008a19d1af7ea3015d5d8138d6c0
Heber <heberlazo@gmail.com>
2020-12-07 14:34:22 -0500

    Fix Linters

--- bin/main.rb
+++ bin/main.rb
@@ -1,37 +1,26 @@
 #!/usr/bin/env ruby
 
 class InitGame
-   
-
-    def Dialogo
-        # Input Players
-        puts 'Whats your name?'
-        @Player_1 = gets.chomp
-
-        puts "Hello #{@Player_1} "
-        puts 'Whats the name of your partner?'
-        @Player_2 = gets.chomp
-
-        puts "Hello #{@Player_2} "
-
-        # Grid Size
-        puts 'Pick a grid size equal or greater than 3'
-        @Pick_size = gets.chomp
-
-        # Building grid to play
-        grilla = Grid.new(@Pick_size) 
-        grilla.create_grid
-        grilla.print_table
-
-        # Turns Methods
-
-        turn = Turn.new(@Player_1, @Player_2)
-        turn.call_turn
-        
-        # Condition to win
-        win = Win.new(@Player_1, @Player_2)
-        win.call_winner
-
-
-    end
+  def ialogo
+    # Input Players
+    puts 'Whats your name?'
+    @player1 = gets.chomp
+    puts "Hello #{@player1} "
+    puts 'Whats the name of your partner?'
+    @player2 = gets.chomp
+    puts "Hello #{@player2} "
+    # Grid Size
+    puts 'Pick a grid size equal or greater than 3'
+    @pick_size = gets.chomp
+    # Building grid to play
+    grilla = Grid.new(@pick_size)
+    grilla.create_grid
+    grilla.print_table
+    # Turns Methods
+    turn = Turn.new(@player1, @player2)
+    turn.call_turn
+    # Condition to win
+    win = Win.new(@player1, @player2)
+    win.call_winner
+  end
 end
