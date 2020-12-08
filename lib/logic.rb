314b38726ee5e4ad3199715e617bf989eb8ff509
Heber <heberlazo@gmail.com>
2020-12-08 15:46:44 -0500

    Add logic method

--- lib/logic.rb
+++ lib/logic.rb
@@ -8,21 +8,27 @@
       if (arr[0]== sim and arr[1]==sim and arr[2]== sim) || (arr[3]== sim and arr[4]== sim and arr[5]== sim) || (arr[6]== sim and arr[7]== sim and arr[8]== sim)
 
         puts "\n !!!!!!!!!!!!#{player} wins !!!!!!!!!!! \n".upcase
+        arr.clear
+        
  
       elsif (arr[0]== sim and arr[3]== sim and arr[6]== sim) || (arr[1]== sim and arr[4]== sim and arr[7]== sim) || (arr[2]== sim and arr[5]== sim and arr[8]== sim)
 
         puts "\n !!!!!!!!!!!!#{player} wins !!!!!!!!!!! \n".upcase   
+        arr.clear
+
 
       elsif (arr[0]== sim and arr[4]== sim and arr[8]== sim) || (arr[2]== sim and arr[4]== sim and arr[6]== sim)
 
         puts "\n !!!!!!!!!!!!#{player} wins !!!!!!!!!!!".upcase
-          
+        arr.clear
+
+
 
       else
 
       end
 
-
+      arr
      end
 
   end
