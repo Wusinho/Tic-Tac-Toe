69cde576b863ffa45647b3275b561e7cda5e1e45
Heber <heberlazo@gmail.com>
2020-12-08 13:01:28 -0500

    Add changes main

--- bin/main.rb
+++ bin/main.rb
@@ -1,9 +1,8 @@
 #!/usr/bin/env ruby
 
+require_relative "../lib/logic"
 
-require_relative "Grid"
-
-class InitGame < Grid
+class InitGame 
     
     def initialize(name1 = 'pepeBOT', name2 = 'raulBOT')
         @turnA = name1
@@ -13,11 +12,8 @@ class InitGame < Grid
      @@grilla = [1,2,3,4,5,6,7,8,9]
      
      def create_grid(arr)
-
-       puts "| #{arr[0]} | #{arr[1]} | #{arr[2]} |"
-       
-       puts "| #{arr[3]} | #{arr[4]} | #{arr[5]} |"
-       
+       puts "| #{arr[0]} | #{arr[1]} | #{arr[2]} |"    
+       puts "| #{arr[3]} | #{arr[4]} | #{arr[5]} |"    
        puts "| #{arr[6]} | #{arr[7]} | #{arr[8]} |"
      end
 
@@ -38,40 +34,21 @@ class InitGame < Grid
         while switch
             if move == true
                 create_grid(@@grilla)
-
                 print "It is #{@Player_1} turn Pick a number : "
                 @turn_01 = gets.chomp.to_i
-                @@grilla[@turn_01] = "X"
-                
-
+                @@grilla[@turn_01] = "X"                
                 move = false
-                
-
             else
                 create_grid(@@grilla)
                 print "It is #{@Player_2} turn Pick a number : "
-
                 @turn_02 = gets.chomp.to_i
-                @@grilla[@turn_02] = "O"
-
-                
+                @@grilla[@turn_02] = "O"              
                 move = true
             end
 
         end
-
-
-
-
-
     end
-
-  
-  
-
-
 end
-
 game = InitGame.new
 game.Dialogo
 #game.change_element(4)
