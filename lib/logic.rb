b94b442c7b384734c21e59e6d756107cca4aeb9d
Heber <heberlazo@gmail.com>
2020-12-08 12:28:58 -0500

    Adding win cond

---
+++ lib/logic.rb
@@ -0,0 +1,26 @@
+
+
+        
+        arr = ["X","X","X",4,5,6,7,8,9]
+        arr2 = []
+
+        #arr[0..2].all? { |a| print "Win " if a == "X" } end
+
+        win = true
+
+        while win
+            
+
+
+            
+        end
+
+        
+        
+        
+
+
+
+
+
+
