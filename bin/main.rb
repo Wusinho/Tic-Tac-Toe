eb7f023744893e050d9c4640e04e8e282b2ae6c5
Miguel Ricaño <mricanho@gmail.com>
2020-12-07 23:13:49 -0600

    Fix mock to display properly

--- bin/main.rb
+++ bin/main.rb
@@ -1,62 +1,72 @@
 #!/usr/bin/env ruby
-# rubocop:disable Lint/NestedMethodDefinition, Metrics/MethodLength
-class InitGame
-  def dialogo
-    # Input Players
-    puts 'Welcome to Tik-tak-toe EXTREME edition'
-    puts 'Whats your name?'
-    @player1 = gets.chomp
-    puts "Hello #{@player1} "
-    puts 'Whats the name of your partner?'
-    @player2 = gets.chomp
-    puts "Hello #{@player2} "
-    # Grid Size
-    puts 'Pick a grid size equal or greater than 3'
-    @pick_size = gets.chomp # The player's pick is 3
-    # Building grid to play
-    grilla = Grid.new(@pick_size)
-    grilla.create_grid
-    grilla.print_table
-    # Starting the game
-    puts 'This is your board'
-    puts [' 1 | 2 | 3 ']
-    puts [' 4 | 5 | 6 ']
-    puts [' 7 | 8 | 9 ']
-    puts "Excelent #{@player1} and #{@player2}, let's begin"
-    # Turns Methods
-    def do_move(move, player, grilla)
-      until ((n..n).include? move) && (grilla.value? move)
-        puts 'Invalid move'
-        puts 'Write another number from the board'
-        move = gets.chomp.to_i
-      end
-      grilla[move] = 'X'
-      print_table(grilla)
-      puts "#{player} it's your turn. Choose one number from the board"
-    end
 
-    # Loop
-    def game_on(_player1, _player2, grilla)
-      # call a method to verify if theres a winner when the conditions are accomplish or if it's a draw
-      # when the loop ends, ask the players if they want to play again or exit the game
-      win = false
-      draw = false
-      until win || draw
-        puts "#{@player1}, it's your turn, choose a number from the board"
-        move = gets.chomp
-        do_move(move, @player1, grilla)
-        puts "#{@player2}, it's your turn, choose a number from the board"
-        move = gets.chomp
-        do_move(move, @player2, grilla)
-        grilla.print_table
-        draw = true
-      end
-      scenarios = { 1 => "#{@player1}, fatality, you won!", 2 => "#{@player2},
-                          fatality, you won!",
-                    3 => "It's a draw!" }
-      result = Random.new.rand(1..3)
-      puts scenarios[result]
-    end
-  end
+grilla = { 1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 9 => 9 }
+SEPARATOR = '-------------'.freeze
+def create_grid(grilla)
+  puts ["| #{grilla[1]} | #{grilla[2]} | #{grilla[3]} |"]
+  puts SEPARATOR
+  puts ["| #{grilla[4]} | #{grilla[5]} | #{grilla[6]} |"]
+  puts SEPARATOR
+  puts ["| #{grilla[7]} | #{grilla[8]} | #{grilla[9]} |"]
 end
-# rubocop:enable Lint/NestedMethodDefinition, Metrics/MethodLength
+
+def do_move(move, _player2, grilla)
+  until ((1..9).include? move) && (grilla.value? move)
+    puts 'Invalid move'
+    puts 'Write another number from the board'
+    move = gets.chomp.to_i
+  end
+  grilla[move] = 'X'
+  print_table(grilla)
+  puts "#{@player1} it's your turn. Choose one number from the board"
+end
+
+# loop
+def game_on(_player1, player2, grilla)
+  # call a method to verify if theres a winner when the conditions are accomplish or if it's a draw
+  # when the loop ends, ask the players if they want to play again or exit the game
+  win = false
+  draw = false
+  until win || draw
+    puts "#{@player1}, it's your turn, choose a number from the board"
+    move = gets.chomp
+    do_move(move, @player1, grilla)
+    puts "#{@player2}, it's your turn, choose a number from the board"
+    move = gets.chomp
+    do_move(move, player2, grilla)
+    grilla.print_table
+    draw = true
+  end
+  scenarios = { 1 => "#{@player1}, fatality, you won!", 2 => "#{@player2},
+                      fatality, you won!",
+                3 => "It's a draw!" }
+  result = Random.new.rand(1..3)
+  puts scenarios[result]
+end
+# mock
+puts 'Welcome to Tik-tak-toe EXTREME edition'
+puts ' '
+puts 'Hey Player One, whats your name?'
+@player1 = gets.chomp
+puts "Hello #{@player1} \n
+Whats the name of the Player Two?"
+@player2 = gets.chomp
+puts "Hello #{@player2}"
+puts ' '
+puts "#{@player1} will be the X and start the game \n
+#{@player2} will be the O and make the second move"
+puts ' '
+puts "This is the board of the game, \n
+Each square has a number and it starts at 1 and ends at 9. \n"
+create_grid(grilla)
+puts 'Write your number to select your move.'
+
+game_on(@player1, @player2, grilla)
+puts 'This is the score'
+puts "#{@player1}: games_won"
+puts "#{@player2}: games_won"
+puts 'Do you want to play another game?'
+puts 'Select yes or no'
+other_game = gets.chomp
+puts "You answer #{other_game}. Let's play again!"
+puts 'See you later!'
