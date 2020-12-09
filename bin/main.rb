bd14fbabb14a3b141b8e04f52ccb5367bea57eb8
Heber <heberlazo@gmail.com>
2020-12-09 00:09:11 -0500

    Fixing number cheat

--- bin/main.rb
+++ bin/main.rb
@@ -12,44 +12,33 @@ class InitGame < WinCondition
     puts "| #{arr[6]} | #{arr[7]} | #{arr[8]} |"
   end
 
-  def cheat_name?(true_name)
-    while true_name == ""
-        puts "Please choose a name"
-        true_name = gets.chomp    
-        
+
+  def my_name(num)
+    return (:my_name) unless num < 10
+
+    while num != (1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9)
+        num = gets.chomp   
     end
-    puts "Hello #{true_name} "
+    self
   end
 
-  def cheat_number?(num)
-    
-    while num != 1 or num != 2 or num != 3 or num != 4 or num != 5 or num != 6 or num != 7 or num != 8 or num != 9  
-        
-        puts "Please choose a number between 1 and 9"
-        num = gets.chomp
-        puts num.class
-    end
-    return num
-  end   
+
 
 
   def dialogo
   win_c = WinCondition.new
     
- 
     puts 'Whats your name?'
-    players[pla1] = gets.chomp
-    cheat_name?(@player1)
-    
-
+    @player1 = gets.chomp 
+    while @player1 == ''
+        @player1 = gets.chomp   
+    end
     puts 'Whats the name of your partner?'
-    players[pla2] = gets.chomp
+    @player2 = gets.chomp
+    while @player2 == ''
+        @player2 = gets.chomp   
+    end
     
-    cheat_name?(@player2)
-
-
-
-    # switch = true
     move = true
 
     while @@grilla != []
@@ -59,9 +48,12 @@ class InitGame < WinCondition
         print "It is #{@player1} turn Pick a number : "
 
         turn1 = gets.chomp.to_i
-        puts turn1.class
-        cheat_number?(turn1)
 
+        while turn1 != (1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9)
+            turn1 = gets.chomp   
+            
+        end
+        turn1.to_int
         @@grilla[turn1 - 1] = 'X'
 
         puts win_c.win(@@grilla, 'X', @player1)
@@ -72,9 +64,13 @@ class InitGame < WinCondition
         create_grid(@@grilla)
         print "It is #{@player2} turn Pick a number : "
 
-        turn2 = gets.chomp
+        turn2 = gets.chomp.to_i
         
-        cheat_number?(turn2)
+        
+        while turn2 != (1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9)
+            turn2 = gets.chomp   
+            
+        end
 
         @@grilla[turn2 - 1] = 'O'
 
