7fac30f2e0c7a0139db420d424e48917268d97fb
Heber <heberlazo@gmail.com>
2020-12-09 12:20:18 -0500

    Fix turns bug

--- bin/main.rb
+++ bin/main.rb
@@ -12,19 +12,6 @@ class InitGame < WinCondition
     puts "| #{arr[6]} | #{arr[7]} | #{arr[8]} |"
   end
 
-
-  def my_name(num)
-    return (:my_name) unless num < 10
-
-    while num != (1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9)
-        num = gets.chomp   
-    end
-    self
-  end
-
-
-
-
   def dialogo
   win_c = WinCondition.new
     
@@ -38,44 +25,38 @@ class InitGame < WinCondition
     while @player2 == ''
         @player2 = gets.chomp   
     end
-    
-    move = true
 
+    move = true
     while @@grilla != []
 
       if move == true
         create_grid(@@grilla)
         print "It is #{@player1} turn Pick a number : "
 
-        turn1 = gets.chomp.to_i
-
-        while turn1 != (1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9)
-            turn1 = gets.chomp   
-            
-        end
-        turn1.to_int
-        @@grilla[turn1 - 1] = 'X'
-
-        puts win_c.win(@@grilla, 'X', @player1)
+        turn1 = gets.chomp       
+          if turn1 =~ /^-?[1-9]+$/
+            turn1 = turn1.to_i
+            turn1 > 10 ?  redo : turn1
+            @@grilla[turn1 - 1] = 'X'
+          else
+            puts "Please choose a number from 0-9"
+            redo  
+          end
+        win_c.win(@@grilla, 'X', @player1)
         move = false
-
       else
-
         create_grid(@@grilla)
-        print "It is #{@player2} turn Pick a number : "
-
-        turn2 = gets.chomp.to_i
-        
-        
-        while turn2 != (1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9)
-            turn2 = gets.chomp   
-            
-        end
-
-        @@grilla[turn2 - 1] = 'O'
-
-        puts win_c.win(@@grilla, 'O', @player2)
-        
+        print "It is #{@player2} turn Pick a number : "   
+           turn2 = gets.chomp
+           if turn2 =~ /^-?[1-9]+$/
+            turn2 = turn2.to_i
+            turn2 > 10 ?  redo : turn2
+            @@grilla[turn2 - 1] = 'O'
+          else
+            puts "Please choose a number from 0-9"
+            redo  
+          end       
+        win_c.win(@@grilla, 'O', @player2)        
         move = true
       end
 
