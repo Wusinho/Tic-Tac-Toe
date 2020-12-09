1fa54ae265a597ae7f644b66a757f832eb08cfc8
Miguel Ricaño <mricanho@gmail.com>
2020-12-09 11:54:34 -0600

    Fix Rubocop errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-# rubocop:disable Style/IdenticalConditionalBranches, Metrics/MethodLength, Style/ClassVars
+# rubocop:disable Style/IdenticalConditionalBranches, Metrics/MethodLength, Style/ClassVars, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/BlockNesting
 
 require_relative '../lib/logic'
 
@@ -13,18 +13,14 @@ class InitGame < WinCondition
   end
 
   def dialogo
-  win_c = WinCondition.new
-    
+    win_c = WinCondition.new
+
     puts 'Whats your name?'
-    @player1 = gets.chomp 
-    while @player1 == ''
-        @player1 = gets.chomp   
-    end
+    @player1 = gets.chomp
+    @player1 = gets.chomp while @player1 == ''
     puts 'Whats the name of your partner?'
     @player2 = gets.chomp
-    while @player2 == ''
-        @player2 = gets.chomp   
-    end
+    @player2 = gets.chomp while @player2 == ''
     move = true
     while @@grilla != []
 
@@ -32,30 +28,30 @@ class InitGame < WinCondition
         create_grid(@@grilla)
         print "It is #{@player1} turn Pick a number : "
 
-        turn1 = gets.chomp       
-          if turn1 =~ /^-?[1-9]+$/
-            turn1 = turn1.to_i
-            turn1 > 10 ?  redo : turn1
-            @@grilla[turn1 - 1] = 'X'
-          else
-            puts "Please choose a number from 0-9"
-            redo  
-          end
+        turn1 = gets.chomp
+        if turn1 =~ /^-?[1-9]+$/
+          turn1 = turn1.to_i
+          turn1 > 10 ? redo : turn1
+          @@grilla[turn1 - 1] = 'X'
+        else
+          puts 'Please choose a number from 0-9'
+          redo
+        end
         win_c.win(@@grilla, 'X', @player1)
         move = false
       else
         create_grid(@@grilla)
-        print "It is #{@player2} turn Pick a number : "   
-           turn2 = gets.chomp
-           if turn2 =~ /^-?[1-9]+$/
-            turn2 = turn2.to_i
-            turn2 > 10 ?  redo : turn2
-            @@grilla[turn2 - 1] = 'O'
-          else
-            puts "Please choose a number from 0-9"
-            redo  
-          end       
-        win_c.win(@@grilla, 'O', @player2)        
+        print "It is #{@player2} turn Pick a number : "
+        turn2 = gets.chomp
+        if turn2 =~ /^-?[1-9]+$/
+          turn2 = turn2.to_i
+          turn2 > 10 ? redo : turn2
+          @@grilla[turn2 - 1] = 'O'
+        else
+          puts 'Please choose a number from 0-9'
+          redo
+        end
+        win_c.win(@@grilla, 'O', @player2)
         move = true
       end
 
@@ -65,4 +61,4 @@ end
 game = InitGame.new
 game.dialogo
 
-# rubocop:enable Style/IdenticalConditionalBranches, Metrics/MethodLength, Style/ClassVars
+# rubocop:enable Style/IdenticalConditionalBranches, Metrics/MethodLength, Style/ClassVars, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/BlockNesting
--- lib/logic.rb
+++ lib/logic.rb
@@ -1,37 +1,19 @@
 # rubocop:disable Layout/LineLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/AbcSize
 class WinCondition
-  def win(arr, sim, player, func = nil)
+  def win(arr, sim, player)
     if (arr[0] == sim and arr[1] == sim and arr[2] == sim) || (arr[3] == sim and arr[4] == sim and arr[5] == sim) || (arr[6] == sim and arr[7] == sim and arr[8] == sim)
       puts "\n !!!!!!!!!!!!#{player} wins !!!!!!!!!!! \n".upcase
-      func
       arr.clear
     elsif (arr[0] == sim and arr[3] == sim and arr[6] == sim) || (arr[1] == sim and arr[4] == sim and arr[7] == sim) || (arr[2] == sim and arr[5] == sim and arr[8] == sim)
-      puts "\n !!!!!!!!!!!!#{player} wins !!!!!!!!!!! \n".upcase  
-      func
+      puts "\n !!!!!!!!!!!!#{player} wins !!!!!!!!!!! \n".upcase
       arr.clear
     elsif (arr[0] == sim and arr[4] == sim and arr[8] == sim) || (arr[2] == sim and arr[4] == sim and arr[6] == sim)
-      puts "\n !!!!!!!!!!!!#{player} wins !!!!!!!!!!!".upcase   
-      func
+      puts "\n !!!!!!!!!!!!#{player} wins !!!!!!!!!!!".upcase
       arr.clear
-    elsif arr.count { |a| a == "X"} == 5
-      puts "DRAW"
-      func
+    elsif arr.count { |a| a == 'X' } == 5
+      puts 'DRAW'
       arr.clear
     end
   end
-  
 end
-
-
-grilla = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-#puts "Enter numeric value: "
-#result = gets.chomp
-
-#if result =~ /[0-9]/
-#    puts "de 0-9"    
-#else
-#    puts 'otros'
-#end
-
-
+# rubocop:enable Layout/LineLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/AbcSize
