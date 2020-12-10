3e361375de72682f4c45c75c2352782825322c7b
Heber <heberlazo@gmail.com>
2020-12-09 21:35:01 -0500

    Add all logics to logic file

--- bin/main.rb
+++ bin/main.rb
@@ -1,10 +1,13 @@
 #!/usr/bin/env ruby
 # rubocop:disable Metrics/MethodLength, Style/ClassVars, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/BlockNesting, Style/IdenticalConditionalBranches
 
-require_relative '../lib/logic'
+require '../lib/logic'
 
-class InitGame < WinCondition
-  @@grilla = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+
+  grilla = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  grilla_close = [0]
 
   def create_grid(arr)
     puts "| #{arr[0]} | #{arr[1]} | #{arr[2]} |"
@@ -12,56 +15,24 @@ class InitGame < WinCondition
     puts "| #{arr[6]} | #{arr[7]} | #{arr[8]} |"
   end
 
-  def dialogo
+  
+
     win_c = WinCondition.new
-    puts "INSTRUCTIONS"
-    puts "TIC TAC TOE"
-    puts "Each player gets a turn"
-    puts "Don't try to cheat or you will get penalized"
-    puts "You will loose a turn"
-    puts 'Whats your name?'
-    @player1 = gets.chomp
-    @player1 = gets.chomp while @player1 == ''
+    
+    puts 'Whats your name'
+    @name1 = gets.chomp
+    @name1 = gets.chomp while @name1 == ''
+  
     puts 'Whats the name of your partner?'
-    @player2 = gets.chomp
-    @player2 = gets.chomp while @player2 == ''
-    move = true
-    while @@grilla != []
-      if move == true
-        create_grid(@@grilla)
-        print "It is #{@player1} turn Pick a number : "
-        turn1 = gets.chomp
-        if turn1 =~ /^-?[1-9]+$/
-          turn1 = turn1.to_i
-          turn1 > 10 ? redo : turn1
-          win_c.change_element(turn1, @@grilla, "X")
-        else
-          puts 'Please choose a number from 1-9'
-          redo
-        end
-        win_c.win(@@grilla, 'X', @player1)
-        move = false
-      else
-        create_grid(@@grilla)
-        print "It is #{@player2} turn Pick a number : "
-        turn2 = gets.chomp
-        if turn2 =~ /^-?[1-9]+$/
-          turn2 = turn2.to_i
-          turn2 > 10 ? redo : turn2
-          win_c.change_element(turn2, @@grilla, "O")
-        else
-          puts 'Please choose a number from 1-9'
-          redo
-        end
-        win_c.win(@@grilla, 'O', @player2)
-        move = true
-      end
-    end
-    puts 'Final Board'
-    create_grid(win_c.final_arr)
-  end
-end
-game = InitGame.new
-game.dialogo
+    @name2 = gets.chomp
+    @name2 = gets.chomp while @name2 == ''
+
+    win_c.logic(grilla_close, grilla, @name1, @name2)  
+  
+
+
+  
+
+
 
 # rubocop:enable Metrics/MethodLength, Style/ClassVars, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/BlockNesting, Style/IdenticalConditionalBranches
--- lib/logic.rb
+++ lib/logic.rb
@@ -1,27 +1,26 @@
-# rubocop:disable Layout/LineLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/AbcSize
+
+#require '../bin/main'
+
+
+
 class WinCondition
-  attr_reader :final_arr
-
-  def initialize
-    @final_arr = []
-  end
-
-  def win(arr, sim, player)
+ 
+  def win(lock, arr, sim, player)
     if (arr[0] == sim and arr[1] == sim and arr[2] == sim) || (arr[3] == sim and arr[4] == sim and arr[5] == sim) || (arr[6] == sim and arr[7] == sim and arr[8] == sim)
       puts "\n !!!!!!!!!!!!#{player} wins !!!!!!!!!!! \n".upcase
-      @final_arr.concat(arr)
-      arr.clear
+      
+      lock.clear
     elsif (arr[0] == sim and arr[3] == sim and arr[6] == sim) || (arr[1] == sim and arr[4] == sim and arr[7] == sim) || (arr[2] == sim and arr[5] == sim and arr[8] == sim)
       puts "\n !!!!!!!!!!!!#{player} wins !!!!!!!!!!! \n".upcase
-      @final_arr.concat(arr)
-      arr.clear
+      
+      lock.clear
     elsif (arr[0] == sim and arr[4] == sim and arr[8] == sim) || (arr[2] == sim and arr[4] == sim and arr[6] == sim)
       puts "\n !!!!!!!!!!!!#{player} wins !!!!!!!!!!!".upcase
-      @final_arr.concat(arr)
-      arr.clear
+      
+      lock.clear
     elsif arr.count { |a| a == 'X' } == 5
       puts 'DRAW'
-      arr.clear
+      lock.clear
     end
   end
 
@@ -32,7 +31,60 @@ class WinCondition
     self
   end  
 
+   def logic(grilla_close, grid, name1, name2)
+    move = true
+    
+    while grilla_close != []
+      if move == true
+        create_grid(grid)
+        print "It is #{name1} turn Pick a number : "
+        turn1 = gets.chomp
+        if turn1 =~ /^-?[1-9]+$/
+          turn1 = turn1.to_i
+          turn1 > 10 ? redo : turn1
+          change_element(turn1, grid, 'X')
+        else
+          redo
+        end
+        win(grilla_close, grid, 'X', name1)
+        
+        move = false
+    
+      else
+    
+        create_grid(grid)
+        print "It is #{name2} turn Pick a number : "
+        turn2 = gets.chomp
+        if turn2 =~ /^-?[1-9]+$/
+          turn2 = turn2.to_i
+          turn2 > 10 ? redo : turn2
+          change_element(turn2, grid, 'O')    
+        else     
+          redo
+        end
+        win(grilla_close, grid, 'O', name2)
+        
+        move = true
+      end
+      
+      
+
+    end
+    create_grid(grid)
+    
+    
+  end  
+
 
 
 end
+
+
+
+
+
+
+
+
+
 # rubocop:enable Layout/LineLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/AbcSize
