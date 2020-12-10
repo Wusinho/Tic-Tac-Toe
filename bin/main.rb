bdd1e20ef9f5c7f3f8ba47a5f2b656cfb1d66082
Heber <heberlazo@gmail.com>
2020-12-10 16:18:48 -0500

    Fix turn loop

--- bin/main.rb
+++ bin/main.rb
@@ -3,10 +3,44 @@
 
 require '../lib/logic'
 
+class Turn
+  
+  def check_turn(turn)
+
+    switch = true
+    while turn != String 
+      
+
+      if turn =~ /^-?[1-9]+$/
+
+        turn = Integer(turn)
+        puts "Pick a number from 1-9"
+        turn = Integer(gets.chomp) while turn > 10
+        
+        puts turn.class
+        return turn
+        
+      else
+        puts "Pick a number from 1-9"
+
+        turn = gets.chomp     
+        redo  
+      end
+      
+       
+    end 
+
+  end
+end
+
+
+
 class Dialogue
   @@grilla = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   grilla_close = [0]
+  posible_move = true
 
+  check = Turn.new
   win_c = WinCondition.new
 
   puts 'Welcome to TIC-TAC-TOE'
@@ -15,28 +49,29 @@ class Dialogue
   puts 'You will loose a turn'
 
   puts 'Whats your name'
-  @playe1_name = gets.chomp
-  @playe1_name = gets.chomp while @playe1_name == ''
+  @player1_name = gets.chomp
+  @player1_name = gets.chomp while @player1_name == ''
 
   puts 'Whats the name of your partner?'
-  @playe2_name = gets.chomp
-  @playe2_name = gets.chomp while @playe2_name == ''
+  @player2_name = gets.chomp
+  @player2_name = gets.chomp while @player2_name == '' 
 
-  posible_move = true
   while grilla_close != []
     if posible_move == true
       print win_c.create_grid(@@grilla)
-      print "It is #{@playe1_name} turn Pick a number : "
-      @playe1_turn = gets.chomp.to_i
-      win_c.change_element(@playe1_turn, @@grilla, 'X')
-      win_c.win(grilla_close, @@grilla, 'X', @playe1_name)
+      print "It is #{@player1_name} turn Pick a number from 1-9 : "
+      @player1_turn = gets.chomp
+     
+      win_c.change_element( check.check_turn(@player1_turn), @@grilla, 'X')
+      win_c.win(grilla_close, @@grilla, 'X', @player1_name)
       posible_move = false
     else
       print win_c.create_grid(@@grilla)
-      print "It is #{@playe2_name} turn Pick a number : "
-      @playe2_turn = gets.chomp.to_i
-      win_c.change_element(@playe2_turn, @@grilla, 'O')
-      win_c.win(grilla_close, @@grilla, 'O', @playe2_name)
+      print "It is #{@player2_name} turn Pick a number from 1-9 : "
+      @player2_turn = gets.chomp
+  
+      win_c.change_element(check.check_turn(@player2_turn), @@grilla, 'O')
+      win_c.win(grilla_close, @@grilla, 'O', @player2_name)
       posible_move = true
     end
   end
--- lib/logic.rb
+++ lib/logic.rb
@@ -14,32 +14,28 @@ class WinCondition
   def win(lock, arr, sim , _player = nil)
     if (arr[0] == sim and arr[1] == sim and arr[2] == sim) || (arr[3] == sim and arr[4] == sim and arr[5] == sim) || (arr[6] == sim and arr[7] == sim and arr[8] == sim)
       lock.clear
-
       return '!!!!!!!!!!!!!!!!!!!YOU WIN!!!!!!!!!!!!!!!!!!!'
-
     elsif (arr[0] == sim and arr[3] == sim and arr[6] == sim) || (arr[1] == sim and arr[4] == sim and arr[7] == sim) || (arr[2] == sim and arr[5] == sim and arr[8] == sim)
       lock.clear
-
       return '!!!!!!!!!!!!!!!!!!!YOU WIN!!!!!!!!!!!!!!!!!!!'
-
     elsif (arr[0] == sim and arr[4] == sim and arr[8] == sim) || (arr[2] == sim and arr[4] == sim and arr[6] == sim)
       lock.clear
-
       return '!!!!!!!!!!!!!!!!!!!YOU WIN!!!!!!!!!!!!!!!!!!!'
-
     elsif arr.count { |a| a == 'X' } == 5
       lock.clear
-
       return 'DRAW'
     end
     '!!!!!!!!!!!!!!!!!!!YOU WIN!!!!!!!!!!!!!!!!!!!'
   end
 
   def change_element(item, array, simbol)
+    
+
+
     array.collect! do |element|
       element == item ? simbol : element
     end
-    self
   end
 end
+
 # rubocop:enable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Layout/LineLength
