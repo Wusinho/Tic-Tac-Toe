5888e6b94e0ec34268def7535dc468ac08d9764b
Miguel Ricaño <mricanho@gmail.com>
2020-12-10 12:08:47 -0600

    Add gemfiles

--- bin/main.rb
+++ bin/main.rb
@@ -1,7 +1,9 @@
 #!/usr/bin/env ruby
-# rubocop:disable Style/ClassVars, Style/IdenticalConditionalBranches
+# rubocop:disable Style/ClassVars, Style/IdenticalConditionalBranches, Layout/LineLength
 
 require '../lib/logic'
+require 'rainbow'
+require 'tty-font'
 
 class Dialogue
   @@grilla = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -9,39 +11,49 @@ class Dialogue
 
   win_c = WinCondition.new
 
-  puts "Welcome to TIC-TAC-TOE"
+  ################################# NEW #####################
+  font = TTY::Font.new(:starwars)
+  puts "\n"
+  puts Rainbow(font.write('Tik-Tak-Toe', letter_spacing: 3)).red.bright
+  puts (Rainbow('CUTE EDITION').blueviolet.bright.blink.bg(:yellow)).center(702)
+  puts "\n"
+  puts (Rainbow("Let's start").blue).center(150)
+  puts "\n"
+  print win_c.create_grid(@@grilla)
+
+  puts "#{Rainbow('this is red').red} and #{Rainbow('this on yellow bg').red.bg(:yellow)} and #{Rainbow('even bright underlined!').red.underline.bright}"
   puts 'Each player gets a turn'
   puts "Don't try to cheat or you will get penalized"
-  puts "You will loose a turn"
+  puts 'You will loose a turn'
 
   puts 'Whats your name'
-  @name1 = gets.chomp
-  @name1 = gets.chomp while @name1 == ''
+  @playe1_name = gets.chomp
+  @playe1_name = gets.chomp while @playe1_name == ''
 
   puts 'Whats the name of your partner?'
-  @name2 = gets.chomp
-  @name2 = gets.chomp while @name2 == ''
+  @playe2_name = gets.chomp
+  @playe2_name = gets.chomp while @playe2_name == ''
 
   posible_move = true
   while grilla_close != []
     if posible_move == true
-      win_c.create_grid(@@grilla)
-      print "It is #{@name1} turn Pick a number : "
-      turn1 = gets.chomp.to_i
-      win_c.change_element(turn1, @@grilla, 'X')
-      win_c.win(grilla_close, @@grilla, 'X', @name1)
+      print win_c.create_grid(@@grilla)
+      print "It is #{@playe1_name} turn Pick a number : "
+      @playe1_turn = gets.chomp.to_i
+      win_c.change_element(@playe1_turn, @@grilla, 'X')
+      win_c.win(grilla_close, @@grilla, 'X', @playe1_name)
       posible_move = false
     else
-      win_c.create_grid(@@grilla)
-      print "It is #{@name2} turn Pick a number : "
-      turn2 = gets.chomp.to_i
-      win_c.change_element(turn2, @@grilla, 'O')
-      win_c.win(grilla_close, @@grilla, 'O', @name2)
+      print win_c.create_grid(@@grilla)
+      print "It is #{@playe2_name} turn Pick a number : "
+      @playe2_turn = gets.chomp.to_i
+      win_c.change_element(@playe2_turn, @@grilla, 'O')
+      win_c.win(grilla_close, @@grilla, 'O', @playe2_name)
       posible_move = true
     end
   end
-  win_c.create_grid(@@grilla)
-  puts win_c.win(grilla_close, @@grilla, 'O')
+  puts win_c.create_grid(@@grilla)
+  puts win_c.win(grilla_close, @@grilla, 'X')
 end
 
-# rubocop:enable Style/ClassVars, Style/IdenticalConditionalBranches
+# rubocop:enable Style/ClassVars, Style/IdenticalConditionalBranches, Layout/LineLength
