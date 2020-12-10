83494108e4e4d777e888a6f30c79dbdf77559c5c
Heber <heberlazo@gmail.com>
2020-12-10 06:43:04 -0500

    discard

--- lib/logic.rb
+++ lib/logic.rb
@@ -11,7 +11,7 @@ class WinCondition
     HEREDOC
   end
 
-  def win(lock, arr, sim, _player = nil)
+  def win(lock, arr, sim , _player = nil)
     if (arr[0] == sim and arr[1] == sim and arr[2] == sim) || (arr[3] == sim and arr[4] == sim and arr[5] == sim) || (arr[6] == sim and arr[7] == sim and arr[8] == sim)
       lock.clear
 
