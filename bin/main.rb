60ac389ff55b6a3b121be5fc083e7a8de6ab5f67
Heber <heberlazo@gmail.com>
2020-12-11 13:04:48 -0500

    Fixing loop

--- bin/main.rb
+++ bin/main.rb
@@ -3,76 +3,46 @@
 
 require_relative '../lib/logic'
 
-class Dialogue
-  def initialize
-    @player1_name = name1
-    @player2_name = name2
-  end
+class Dialogue 
 
-  def self.get_input
-      @item = gets.chomp.to_i 
-  end
+
+  
+    player1 = {
+      :name => 'botPaco',
+      :simbol => 'X'
+    }
+  
+    player2 = {
+      :name => 'botLucho',
+      :simbol => '-'
+    }
+  
 
   @grilla = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  grilla_close = [0]
-  posible_move = true
+ 
 
 
-  win_c = WinCondition.new
-  sim_c = Loop.new
+  
+  
 
-  puts 'Welcome to TIC-TAC-TOE'
-  puts 'Each player gets a turn'
-  puts "Don't try to cheat or you will get penalized"
-  puts 'You will loose a turn'
+ 
+    
+def self.user_input(player)
 
-  puts 'Whats your name'
-  @player1_name = gets.chomp
-  @player1_name = gets.chomp while @player1_name == ''
+    puts "Please type a name? else will be #{player[:name]}"
+    player[:name] = gets.chomp
+    #player[:name] = gets.chomp while @player[:name] == ''
 
-  puts 'Whats the name of your partner?'
-  @player2_name = gets.chomp
-  @player2_name = gets.chomp while @player2_name == ''
+  
+    
+end
+sim_c = Loop.new(user_input(player1),   user_input(player2))
 
-  while grilla_close != []
-    if posible_move == true
-      print win_c.create_grid(@grilla)
-      print "It is #{@player1_name} turn Pick a number from 1-9 : "
 
-      get_input
-      
 
-        if @grilla.include?(@item)
-          sim_c.sim_change(@item, @grilla, 'X')
-        else
-          puts "#{@player1_name} pick a number between 1-9 diffrente than #{@item} "
-        end
-      
 
-      win_c.win(grilla_close, @grilla, 'X', @player2_name)
 
-      posible_move = false
-
-    else
-      print win_c.create_grid(@grilla)
-      print "It is #{@player2_name} turn Pick a number from 1-9 : "
-
-        
-        
-          if @grilla.include?(@item)
-          sim_c.sim_change(@item, @grilla, 'O')
-        else
-          puts "#{@player1_name} pick a number between 1-9 diffrente than #{@item} "
-          
-        end
-      
-
-      win_c.win(grilla_close, @grilla, '-', @player2_name)
-      posible_move = true
-    end
-  end
-  puts win_c.create_grid(@grilla)
-  puts win_c.win(grilla_close, @grilla, 'X')
+  
 end
 
 # rubocop:enable Style/IdenticalConditionalBranches
