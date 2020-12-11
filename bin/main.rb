7a63c36713f0aefbc88537597cc4a9ef3b84993c
Miguel Ricaño <mricanho@gmail.com>
2020-12-11 16:41:07 -0600

    Fix minor details

--- bin/main.rb
+++ bin/main.rb
@@ -1,92 +1,58 @@
 #!/usr/bin/env ruby
-# rubocop:disable Style/IdenticalConditionalBranches
 
 require_relative '../lib/logic'
 
-class Dialogue 
+class Dialogue
+  @player1 = {
+    name: 'Player 1',
+    simbol: 'X',
+    number: 0
+  }
 
-    @player1 = {
-      :name => 'botPaco',
-      :simbol => 'X',
-      :number => 0
-    }
-  
-    @player2 = {
-      :name => 'botLucho',
-      :simbol => '-',
-      :number => 0
-    }
-  
+  @player2 = {
+    name: 'Player 2',
+    simbol: 'O',
+    number: 0
+  }
+
   @grilla = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  @checkout = ["este es un array para vacear"]
-  
+  @checkout = ['Tic-tac-toe']
+
   puts @checkout
   @board = Board.new
   @check = ChangeNumbers.new
   @win = WinCondition.new
   @draw = DrawCondition.new
-  
-      puts "Please type a name? else will be #{@player1[:name]}"
-      @player1[:name] = gets.chomp
-      @player1[:name] = gets.chomp while @player1[:name] == ''
-      
-      puts "Please type a name? else will be #{@player2[:name]}"
-      @player2[:name] = gets.chomp
-      @player2[:name] = gets.chomp while @player2[:name] == ''
 
+  puts "Please type a name? else will be #{@player1[:name]}"
+  @player1[:name] = gets.chomp
+  @player1[:name] = gets.chomp while @player1[:name] == ''
 
-      puts @board.board_game(@grilla)
+  puts "Please type a name? else will be #{@player2[:name]}"
+  @player2[:name] = gets.chomp
+  @player2[:name] = gets.chomp while @player2[:name] == ''
+
+  puts @board.board_game(@grilla)
 
   def self.turns
     @turn_count = 0
-
     while @checkout != []
       @turn_count += 1
-     
-    @current_player = @turn_count.odd? ? @player1 : @player2
-    print "#{@current_player[:name]}, Please choose a number between 1-9: "  
-      
-
+      @current_player = @turn_count.odd? ? @player1 : @player2
+      print "#{@current_player[:name]}, Please choose a number between 1-9: "
       @current_player[:number] = gets.chomp.to_i
-      
-
       if @grilla.include?(@current_player[:number])
-        
-
         @check.number_change(@current_player[:number], @grilla, @current_player[:simbol])
         puts @board.board_game(@grilla)
-
-        
         @win.win(@grilla, @current_player[:name], @current_player[:simbol], @checkout)
-        
         @draw.draw(@grilla, @current_player[:name], @current_player[:simbol], @checkout)
-
-        if @checkout == []
-        
-        else
-
-        
-
-        end
-
       else
-      
-        turns   
+        turns
       end
-      
-
     end
   end
-
-
- 
-      
   turns
 
-  
-  puts  @win.win(@grilla, @current_player[:name], @current_player[:simbol], @checkout)
+  puts @win.win(@grilla, @current_player[:name], @current_player[:simbol], @checkout)
   puts @draw.draw(@grilla, @current_player[:name], @current_player[:simbol], @checkout)
-
 end
-
-# rubocop:enable Style/IdenticalConditionalBranches
--- lib/logic.rb
+++ lib/logic.rb
@@ -1,7 +1,5 @@
-
 # require '../bin/main'
-# rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Layout/LineLength, Lint/LiteralInInterpolation
-  
+# rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Layout/LineLength, Style/GuardClause
 
 class Board
   def board_game(arr)
@@ -15,57 +13,36 @@ class Board
   end
 end
 
-
 class WinCondition
-
   def win(arr, player, sim, checkout)
     if (arr[0] == sim and arr[1] == sim and arr[2] == sim) || (arr[3] == sim and arr[4] == sim and arr[5] == sim) || (arr[6] == sim and arr[7] == sim and arr[8] == sim)
-      checkout.clear     
-      return "#{player} WINS"
+      checkout.clear
+      "#{player} WINS"
     elsif (arr[0] == sim and arr[3] == sim and arr[6] == sim) || (arr[1] == sim and arr[4] == sim and arr[7] == sim) || (arr[2] == sim and arr[5] == sim and arr[8] == sim)
       checkout.clear
-      return "#{player} WINS"
+      "#{player} WINS"
     elsif (arr[0] == sim and arr[4] == sim and arr[8] == sim) || (arr[2] == sim and arr[4] == sim and arr[6] == sim)
       checkout.clear
-      return "#{player} WINS"  
-    else
+      "#{player} WINS"
     end
-  end 
+  end
 end
 
 class DrawCondition
-  def draw(arr, sim, player, checkout)
-    if arr.count { |a| a == 'X' } == 5   
-    checkout.clear
-     return "IT'S A DRAW"
+  def draw(arr, _sim, _player, checkout)
+    if arr.count { |a| a == 'X' } == 5
+      checkout.clear
+      "IT'S A DRAW"
     end
- end
+  end
 end
 
 class ChangeNumbers
-  def number_change(item ,arr, sim)
-      arr.collect! do |element|
-        element == item ? sim : element
-      end
+  def number_change(item, arr, sim)
+    arr.collect! do |element|
+      element == item ? sim : element
     end
+  end
 end
 
-#turno = Loop.new
-#turno.turns('Pedro', 'ablo')
-
-
-
-
-
- 
-
-
-
-
-# rubocop:enable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Layout/LineLength, Lint/LiteralInInterpolation
-
-
-
-
-
-
+# rubocop:enable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Layout/LineLength, Style/GuardClause
