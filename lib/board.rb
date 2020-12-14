be529f0f2932e1a4e114088287a294ebf5abcef9
Heber <heberlazo@gmail.com>
2020-12-13 23:10:32 -0500

    discard

--- lib/board.rb
+++ lib/board.rb
@@ -1,34 +1,56 @@
 # rubocop:disable Lint/LiteralInInterpolation
 class Board
-  @new_board = []
+  def create_board(arr, board )
+    arr = (1...board*board+1).to_a        
 
-  def self.initialize
-    @board = grid.to_i   
-    @new_board = []
+    
+
   end
 
-
-  
-  def self.board_game(arr, board, new_board)
-    #@new_board = []
-      arr = (0...@board**2).to_a        
+  def board_game(arr, board, new_board)
+    
+      arr = (0...board*board).to_a        
       until arr == []
-        @new_board += [arr[0..@board-1]]
-          arr.shift(@board)
+        new_board += [arr[0..board-1]]
+          arr.shift(board)
       end 
-      arr = (0...@board**2).to_a        
-      return @new_board
+          
+      new_board
   end
 
 
 end
 
 
-class ChangeNumbers
-  def number_change(item, arr, sim)
-    arr.collect! do |element|
-      element == item ? sim : element
+
+
+
+
+def print_table(arr, margin_width = 2)
+
+  column_widths = []
+  arr.each do |row|
+    row.each.with_index do |cell, column_num|
+      column_widths[column_num] = [column_widths[column_num] || 0, cell.to_s.size].max
     end
   end
+
+  puts (arr.collect do |row|
+    row.collect.with_index do |cell, column_num|
+      cell.to_s.ljust(column_widths[column_num] + margin_width)
+    end.join
+  end)
 end
-# rubocop:enable Lint/LiteralInInterpolation
+
+
+  class ChangeNumbers
+
+    def number_change(item, arr, sim)
+      arr.collect! do |element|
+        element == item ? sim : element
+      end
+    end
+    
+  end
+
+  # rubocop:enable Lint/LiteralInInterpolation
