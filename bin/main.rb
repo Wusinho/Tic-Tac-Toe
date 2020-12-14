69f88a186782f1471e2dbd375626d21d603e4d7e
Heber <heberlazo@gmail.com>
2020-12-13 23:10:11 -0500

    Fix create Board bug not creating it

--- bin/main.rb
+++ bin/main.rb
@@ -18,17 +18,18 @@ require_relative '../lib/board'
 puts @checkout
 @turn_count = 0
 @board_display = Board.new
-@check = ChangeNumbers.new
+#@check = ChangeNumbers.new
 @analize = Analize.new
 
 puts "Please type the size of the board? "
 @board = gets.chomp.to_i
-puts @board.class
+puts @new_board.class
 
-@board_display.board_game(@grilla, @board, @new_board)
-
+@board_display.create_board(@grilla, @board)
 print @grilla
-print @new_board
+
+
+
 
 puts "Please type a name? or it will be #{@player1[:name]}"
 @player1[:name] = gets.chomp
@@ -36,7 +37,7 @@ puts "Please type a name? or it will be #{@player1[:name]}"
 puts "Please type a name? or it will be #{@player2[:name]}"
 @player2[:name] = gets.chomp
 @player2[:name] = gets.chomp while @player2[:name] == ''
-puts @board.board_game(@grilla)
+#
 
 def self.turns
   while @checkout != []
@@ -45,7 +46,7 @@ def self.turns
     @current_player[:number] = gets.chomp.to_i
     if @grilla.include?(@current_player[:number])
       @check.number_change(@current_player[:number], @grilla, @current_player[:simbol])
-      puts @board.board_game(@grilla)
+ #     puts @board.board_game(@grilla)
       @analize.check_rows(@grilla, 3, @current_player[:name], @current_player[:simbol], @checkout)
       @analize.check_collumns(@grilla, 3, @current_player[:name], @current_player[:simbol], @checkout)
       @analize.check_cross_right(@grilla, 3, @current_player[:name], @current_player[:simbol], @checkout)
@@ -56,17 +57,17 @@ def self.turns
 
       
     else
-      puts @board.board_game(@grilla)
+  #    puts @board.board_game(@grilla)
       puts "The position has already been taken, please choose another number #{@current_player[:number]}"
       turns
     end
   end
 end
-turns
 
-puts @analize.check_rows(@grilla, 3, @current_player[:name], @current_player[:simbol], @checkout)
-puts @analize.check_collumns(@grilla, 3, @current_player[:name], @current_player[:simbol], @checkout)
-puts @analize.check_cross_right(@grilla, 3, @current_player[:name], @current_player[:simbol], @checkout)
-puts @analize.check_cross_left(@grilla, 3, @current_player[:name], @current_player[:simbol], @checkout)
-puts @analize.draw(@grilla, 3, @current_player[:name], @current_player[:simbol], @checkout)
+
+#puts @analize.check_rows(@grilla, 3, @current_player[:name], @current_player[:simbol], @checkout)
+#puts @analize.check_collumns(@grilla, 3, @current_player[:name], @current_player[:simbol], @checkout)
+#puts @analize.check_cross_right(@grilla, 3, @current_player[:name], @current_player[:simbol], @checkout)
+#puts @analize.check_cross_left(@grilla, 3, @current_player[:name], @current_player[:simbol], @checkout)
+#puts @analize.draw(@grilla, 3, @current_player[:name], @current_player[:simbol], @checkout)
 
