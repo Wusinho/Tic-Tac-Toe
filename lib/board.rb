f2c73dc588f4d530426eb0e71112656cab970694
Heber <heberlazo@gmail.com>
2020-12-16 17:46:49 -0500

    Fixed linters

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,9 @@
 class Board
   def board_game(arr)
     <<~HEREDOC
-      #{'    '}
           | #{arr[0]} | #{arr[1]} | #{arr[2]} |
           | #{arr[3]} | #{arr[4]} | #{arr[5]} |
           | #{arr[6]} | #{arr[7]} | #{arr[8]} |
-      #{'    '}
     HEREDOC
   end
 end
