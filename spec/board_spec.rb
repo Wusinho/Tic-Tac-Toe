e7792056f664ab76e53498848b1ccdee8dd08025
Heber <heberlazo@gmail.com>
2020-12-17 14:10:45 -0500

    discard

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,5 +1,32 @@
-require '../lib/board'
+require_relative '../lib/board'
+require_relative '../lib/logic'
 
-Describe Board
+# describe ChangeNumbers do
+#   describe 'number_change:' do
+#     it 'change an element from the array with a symbol' do
+#       change = ChangeNumbers.new
+#       expect(change.number_change(2, [1, 2, 3, 4, 5, 6, 7, 8, 9], 'X')).to eql([1, 'X', 3, 4, 5, 6, 7, 8, 9])
+#     end
+#   end
+# end
 
+describe Board do
+  describe 'create_board:' do
+    let(:board_game) { Board.new }
+    let(:arr) { [] }
+    let(:board) { 3 }
+    let(:new_arr) { [] }
+    
+    
 
+    it 'creates a normal array' do
+      expect(board_game.create_board(arr, board)).to eql([1,2,3,4,5,6,7,8,9])
+    end
+
+    it 'creates a normal 2D array' do
+        expect(board_game.board_game(arr, board, new_arr)).to eql([[1,2,3],[4,5,6],[7,8,9]])
+      end
+
+  
+  end
+end
