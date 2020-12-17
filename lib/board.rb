2591fd9d4895b07d766684b494307c359e1021d2
Heber <heberlazo@gmail.com>
2020-12-17 14:10:30 -0500

    Fix loop bug

--- lib/board.rb
+++ lib/board.rb
@@ -1,63 +1,89 @@
 # rubocop:disable Lint/LiteralInInterpolation
 class Board
-  def create_board(arr, board )
-    arr = (1...board*board+1).to_a        
-    arr
-    print arr
-
+  def create_board(board)
+    @arr = (1...board*board+1).to_a     
+    print @arr   
   end
+  @arr = []
 
-  def board_game(arr, board, new_board)
-      arr = (0...board*board).to_a        
-      until arr == []
-        new_board += [arr[0..board-1]]
-          arr.shift(board)
-      end 
+  def board_game
+    puts 'size?'
+    size = gets.chomp.to_i
+    create_board(size)
+      # new_board = []
+      # arr = (0...board*board).to_a        
+      # until arr == []
+      #   new_board += [arr[0..board-1]]
+      #     arr.shift(board)
+      # end 
           
-      new_board
+      # arr << new_board
+      # print new_board
+      # print arr
   end
+
 end
 
-arr = []
 new_arr = []
-puts 'size?'
-size = gets.chomp.to_i
+
 
 nuevo = Board.new
 
-nuevo.create_board(arr, size)
-
-nuevo.board_game(arr, size, new_arr )
 
 
+#nuevo.board_game
 
 
+arr = [1,2,3,4,5,6,7,8,9]
 
-def print_table(arr, margin_width = 2)
-
-  column_widths = []
-  arr.each do |row|
-    row.each.with_index do |cell, column_num|
-      column_widths[column_num] = [column_widths[column_num] || 0, cell.to_s.size].max
-    end
-  end
-
-  puts (arr.collect do |row|
-    row.collect.with_index do |cell, column_num|
-      cell.to_s.ljust(column_widths[column_num] + margin_width)
-    end.join
-  end)
-end
-
-
-  class ChangeNumbers
-
-    def number_change(item, arr, sim)
-      arr.collect! do |element|
-        element == item ? sim : element
-      end
-    end
+def selection (array)
+  #arr2D = []
+count = 0
+  while count < 9
+    array << array[count..(2+count)]
+    array.drop(3)
+    count += 3
+    
     
   end
+  #array.delete(9)
+  print array
 
-  # rubocop:enable Lint/LiteralInInterpolation
+  #print array.drop(9)
+   #print array
+
+end
+
+#arr = [ [1,2,3], [4,5,6], [7,8,9]]
+
+selection(arr)
+
+
+# def print_table(arr, margin_width = 2)
+
+#   column_widths = []
+#   arr.each do |row|
+#     row.each.with_index do |cell, column_num|
+#       column_widths[column_num] = [column_widths[column_num] || 0, cell.to_s.size].max
+#     end
+#   end
+
+#   puts (arr.collect do |row|
+#     row.collect.with_index do |cell, column_num|
+#       cell.to_s.ljust(column_widths[column_num] + margin_width)
+#     end.join
+#   end)
+# end
+
+
+
+
+#     def number_change(item, arr, sim)
+#       arr.collect! do |element|
+#         element == item ? sim : element
+#       end
+#     end
+    
+
+   
+#   # rubocop:enable Lint/LiteralInInterpolation
